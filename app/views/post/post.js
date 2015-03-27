f0aeee5492b5a6a1c3aed890836605babaabab43
bremRobert <brem_robert@hotmail.com>
2015-03-27 21:43:11 +0100

    working redirect

--- app/views/post/post.js
+++ app/views/post/post.js
@@ -1,16 +1,16 @@
 'use strict';
 
-var postApp = angular.module('myApp.post', [ 'ngRoute' ]);
+var postApp = angular.module('myApp.post', ['ngRoute']);
 
-postApp.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/post', {
-    templateUrl: 'views/post/post.html',
-    controller: 'PostCtrl'
-  });
+postApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/post/:id', {
+        templateUrl: 'views/post/post.html',
+        controller: 'PostCtrl'
+    });
 }]);
 
-postApp.controller('PostCtrl', function($scope, $http) {
-  $http.get("http://104.167.98.125:8080/entryservice/resources/entries/1").then(function(response) {
-    $scope.entry = response.data;
-  });
+postApp.controller('PostCtrl', function ($scope, $http, $routeParams) {
+    $http.get("http://104.167.98.125:8080/entryservice/resources/entries/" + $routeParams.id).then(function (response) {
+        $scope.entry = response.data;
+    });
 });
